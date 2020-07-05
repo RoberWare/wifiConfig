a2c833ba12388f4768b4187fda187333699abae7
Roberto Lama <roberlama@gmail.com>
2020-07-05 18:14:34 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -9,8 +9,7 @@ else:
 
 s = setup(
     install_requires=[
-        'flask',
-        'multiprocessing'
+        'flask'
     ],
     classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
